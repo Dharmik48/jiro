3d74a23f884f424e146c5e8539fbec01c7b66d95
dharmik48 <hingudharmik4@gmail.com>
2024-07-24 20:21:34 +0530

    fix: move provider in client component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from 'next'
 import { Oxygen } from 'next/font/google'
 import './globals.css'
-import { ClerkProvider, useAuth } from '@clerk/nextjs'
-import { ConvexProviderWithClerk } from 'convex/react-clerk'
-import { ConvexReactClient } from 'convex/react'
+import { ConvexClientProvider } from '@/providers/ConvexClientProvider'
 
 const oxygen = Oxygen({ weight: ['400', '700'], subsets: ['latin'] })
 
@@ -13,8 +11,6 @@ export const metadata: Metadata = {
 		'Discover Jiro, the ultimate platform for collaborative teamwork and task planning. Create interactive boards to streamline projects, share ideas in real-time, and achieve seamless productivity. Start collaborating effortlessly with Jiro today!',
 }
 
-const convex = new ConvexReactClient(process.env.CONVEX_DEPLOYMENT as string)
-
 export default function RootLayout({
 	children,
 }: Readonly<{
@@ -23,13 +19,7 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<body className={oxygen.className}>
-				<ClerkProvider
-					publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-				>
-					<ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-						{children}
-					</ConvexProviderWithClerk>
-				</ClerkProvider>
+				<ConvexClientProvider>{children}</ConvexClientProvider>
 			</body>
 		</html>
 	)
