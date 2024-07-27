f776bbe85950fbc8912a2fbc808d305a31936a06
dharmik48 <hingudharmik4@gmail.com>
2024-07-27 13:11:19 +0530

    fix: make heading link

--- components/OrganizationSidebar.tsx
+++ components/OrganizationSidebar.tsx
@@ -2,14 +2,17 @@ import { OrganizationSwitcher } from '@clerk/nextjs'
 import Image from 'next/image'
 
 import BoardsTypesSwitcher from './BoardsTypeSwitcher'
+import Link from 'next/link'
 
 const OrganizationSidebar = () => {
 	return (
 		<div className='p-4 bg-secondary min-w-52 lg:flex flex-col shadow-sm h-full gap-8 hidden'>
-			<div className='flex items-center gap-4'>
-				<Image src={'/logo.svg'} alt='Jiro Logo' width={40} height={40} />
-				<h1 className='text-3xl font-bold tracking-wider'>Jiro</h1>
-			</div>
+			<Link href={'/'}>
+				<div className='flex items-center gap-4'>
+					<Image src={'/logo.svg'} alt='Jiro Logo' width={40} height={40} />
+					<h1 className='text-3xl font-bold tracking-wider'>Jiro</h1>
+				</div>
+			</Link>
 			<OrganizationSwitcher
 				hidePersonal
 				appearance={{
