c9719c20d8d80fa004651d4cd66a31ccdf902edb
dharmik48 <hingudharmik4@gmail.com>
2024-07-28 16:17:35 +0530

    feat: adds clear btn in search

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,12 +7,14 @@ import {
 	UserButton,
 } from '@clerk/nextjs'
 import { Input } from './ui/input'
-import { Search } from 'lucide-react'
+import { Search, X } from 'lucide-react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import { useDebounceValue } from 'usehooks-ts'
 import queryString from 'query-string'
 import OrganizationInvite from './OrganizationInvite'
+import { cn } from '@/lib/utils'
+import { Button } from './ui/button'
 
 const NavBar = () => {
 	const router = useRouter()
@@ -44,7 +46,22 @@ const NavBar = () => {
 		<nav className='flex py-4 lg:px-8 px-4 gap-4 items-center justify-between w-full self-start'>
 			<div className='relative w-full max-w-lg hidden lg:block'>
 				<Search className='absolute left-2 top-1/2 -translate-y-1/2 text-muted h-4 w-4' />
-				<Input placeholder='Search' className='pl-8' onChange={handleChange} />
+				<Input
+					placeholder='Search'
+					className='px-8'
+					value={value}
+					onChange={handleChange}
+				/>
+				<Button
+					variant={'ghost'}
+					className={cn(
+						'absolute right-2 p-0 hover:bg-transparent top-1/2 hidden -translate-y-1/2 text-muted h-4 w-4',
+						value.length > 0 && 'block'
+					)}
+					onClick={() => setValue('')}
+				>
+					<X className='h-4 w-4' />
+				</Button>
 			</div>
 			<OrganizationSwitcher
 				hidePersonal
